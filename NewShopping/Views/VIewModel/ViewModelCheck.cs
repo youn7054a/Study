8882b84316e5f54fdabc940f82ae6a1af54083fd
youn7054a <youn7054@gmail.com>
2020-08-26 15:03:20 +0900

    체크뷰모델추가
    추가

---
+++ NewShopping/Views/VIewModel/ViewModelCheck.cs
@@ -0,0 +1,14 @@
+﻿using ComModel;
+using SHOP.Model;
+using System;
+using System.Collections.Generic;
+
+namespace ZNewShopping.Views.VIewModel
+{
+    public class ViewModelCheck
+    {
+        public Checkout Checkout { get; set; }
+        public ShipInfo ShipInfo { get; set; }
+    }
+}
+ 
